dbe8ee59626a5feb9975557898613b95607f1948
Stephen Gold <sgold@sonic.net>
2020-02-03 19:33:33 -0800

    bump version to 5.0.0for33 and update release-notes.md

--- HeartLibrary/src/main/java/jme3utilities/Heart.java
+++ HeartLibrary/src/main/java/jme3utilities/Heart.java
@@ -362,7 +362,7 @@ public class Heart {
      * @return project name, library name, branch, and revision
      */
     public static String version() {
-        return "Heart Heart master $Rev: 4.5.0for33+1 $";
+        return "Heart Heart master $Rev: 5.0.0for33 $";
     }
 
     /**
