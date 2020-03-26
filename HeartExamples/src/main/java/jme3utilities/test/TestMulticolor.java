ec4b48d5f9e63e27c636cce18b88f900719dff85
Stephen Gold <sgold@sonic.net>
2020-03-25 19:37:18 -0700

    add TestMulticolor application

---
+++ HeartExamples/src/main/java/jme3utilities/test/TestMulticolor.java
@@ -0,0 +1,132 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.test;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer;
+import com.jme3.system.AppSettings;
+import com.jme3.util.BufferUtils;
+import java.nio.FloatBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Heart;
+import jme3utilities.debug.Dumper;
+import jme3utilities.mesh.RectangleMesh;
+
+/**
+ * A SimpleApplication to test "multicolor.j3md". If successful, a colorful
+ * wireframe quad will be displayed.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestMulticolor extends SimpleApplication {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger
+            = Logger.getLogger(TestMulticolor.class.getName());
+    /**
+     * application name (for the title bar of the app's window)
+     */
+    final private static String applicationName
+            = TestMulticolor.class.getSimpleName();
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        /*
+         * Mute the chatty loggers found in some imported packages.
+         */
+        Heart.setLoggingLevels(Level.WARNING);
+
+        TestMulticolor app = new TestMulticolor();
+        /*
+         * Customize the window's title bar.
+         */
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        settings.setTitle(applicationName);
+
+        settings.setRenderer(AppSettings.LWJGL_OPENGL32);
+        app.setSettings(settings);
+        app.start();
+        /*
+         * ... and onward to TestMulticolor.simpleInitApp()!
+         */
+    }
+    // *************************************************************************
+    // SimpleApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void simpleInitApp() {
+        flyCam.setEnabled(false);
+        cam.setLocation(new Vector3f(0f, 0f, 4f));
+        cam.setName("cam");
+
+        Mesh mesh = new RectangleMesh();
+        /*
+         * Add a Color buffer to the Mesh.
+         */
+        ColorRGBA[] colorArray = new ColorRGBA[]{
+            ColorRGBA.Red,
+            ColorRGBA.Green,
+            ColorRGBA.Blue,
+            ColorRGBA.White
+        };
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(colorArray);
+        mesh.setBuffer(VertexBuffer.Type.Color, 4, buffer);
+        Geometry quadGeometry = new Geometry("quad", mesh);
+
+        Material testMaterial = new Material(assetManager,
+                "MatDefs/wireframe/multicolor.j3md");
+        quadGeometry.setMaterial(testMaterial);
+        testMaterial.setName("testMaterial");
+
+        rootNode.attachChild(quadGeometry);
+
+        Dumper dumper = new Dumper();
+        dumper.setDumpMatParam(true);
+        dumper.setDumpTransform(true);
+        dumper.dump(rootNode);
+    }
+}
