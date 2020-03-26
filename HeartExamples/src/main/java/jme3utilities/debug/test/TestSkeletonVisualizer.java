570469a28c7cff1ff6362da00e3d93c43039f511
Stephen Gold <sgold@sonic.net>
2020-03-25 19:20:43 -0700

    add TestSkeletonVisualizer application

---
+++ HeartExamples/src/main/java/jme3utilities/debug/test/TestSkeletonVisualizer.java
@@ -0,0 +1,349 @@
+/*
+ Copyright (c) 2020, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.debug.test;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.SkeletonControl;
+import com.jme3.font.BitmapText;
+import com.jme3.font.Rectangle;
+import com.jme3.input.CameraInput;
+import com.jme3.input.KeyInput;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.system.AppSettings;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Heart;
+import jme3utilities.InfluenceUtil;
+import jme3utilities.MyAsset;
+import jme3utilities.MyCamera;
+import jme3utilities.debug.Dumper;
+import jme3utilities.debug.SkeletonVisualizer;
+import jme3utilities.mesh.RectangleMesh;
+import jme3utilities.ui.ActionApplication;
+import jme3utilities.ui.CameraOrbitAppState;
+import jme3utilities.ui.HelpUtils;
+import jme3utilities.ui.InputMode;
+
+/**
+ * An ActionApplication to test SkeletonVisualizer.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TestSkeletonVisualizer extends ActionApplication {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(TestSkeletonVisualizer.class.getName());
+    /**
+     * application name (for the title bar of the app's window)
+     */
+    final private static String applicationName
+            = TestSkeletonVisualizer.class.getSimpleName();
+    // *************************************************************************
+    // fields
+
+    /**
+     * status displayed in the upper-left corner of the GUI node
+     */
+    private BitmapText statusLine;
+    /**
+     * dump debugging information to System.out
+     */
+    final private Dumper dumper = new Dumper();
+    /**
+     * GUI node for displaying hotkey help/hints
+     */
+    private Node helpNode;
+    /**
+     * visualizer for the skeleton of the C-G model
+     */
+    private SkeletonVisualizer sv;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the TestSkeletonVisualizer application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        /*
+         * Mute the chatty loggers found in some imported packages.
+         */
+        Heart.setLoggingLevels(Level.WARNING);
+
+        TestSkeletonVisualizer application = new TestSkeletonVisualizer();
+        /*
+         * Customize the window's title bar.
+         */
+        boolean loadDefaults = true;
+        AppSettings settings = new AppSettings(loadDefaults);
+        settings.setTitle(applicationName);
+
+        application.setSettings(settings);
+        application.start();
+        /*
+         * ... and onward to TestSkeletonVisualizer.actionInitializeApplication()!
+         */
+    }
+    // *************************************************************************
+    // ActionApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void actionInitializeApplication() {
+        configureCamera();
+        configureDumper();
+
+        ColorRGBA bgColor = new ColorRGBA(0.2f, 0.2f, 0.2f, 1f);
+        viewPort.setBackgroundColor(bgColor);
+
+        addFloor();
+        addLighting();
+        rootNode.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        /*
+         * Add the status text to the GUI.
+         */
+        statusLine = new BitmapText(guiFont);
+        statusLine.setLocalTranslation(0f, cam.getHeight(), 0f);
+        guiNode.attachChild(statusLine);
+
+        Spatial jaime = assetManager.loadModel("Models/Jaime/Jaime.j3o");
+        rootNode.attachChild(jaime);
+
+        SkeletonControl sc = jaime.getControl(SkeletonControl.class);
+        AnimControl animControl
+                = jaime.getControl(AnimControl.class);
+        AnimChannel animChannel = animControl.createChannel();
+        animChannel.setAnim("Punches");
+
+        sv = new SkeletonVisualizer(assetManager, sc);
+        sv.setLineColor(ColorRGBA.Yellow);
+        /*
+         * Clean up Jaime's skeleton visualization by hiding the "IK" bones,
+         * which don't influence any mesh vertices.
+         */
+        InfluenceUtil.hideNonInfluencers(sv, sc);
+        jaime.addControl(sv);
+        sv.setEnabled(true);
+    }
+
+    /**
+     * Add application-specific hotkey bindings and override existing ones.
+     */
+    @Override
+    public void moreDefaultBindings() {
+        InputMode dim = getDefaultInputMode();
+
+        dim.bind("dump viewport", KeyInput.KEY_P);
+
+        dim.bind("signal " + CameraInput.FLYCAM_LOWER, KeyInput.KEY_DOWN);
+        dim.bind("signal " + CameraInput.FLYCAM_RISE, KeyInput.KEY_UP);
+        dim.bind("signal orbitLeft", KeyInput.KEY_LEFT);
+        dim.bind("signal orbitRight", KeyInput.KEY_RIGHT);
+
+        dim.bind("toggle help", KeyInput.KEY_H);
+        dim.bind("toggle pause", KeyInput.KEY_PERIOD);
+        dim.bind("toggle skeleton", KeyInput.KEY_V);
+
+        float x = 10f;
+        float y = cam.getHeight() - 30f;
+        float width = cam.getWidth() - 20f;
+        float height = cam.getHeight() - 20f;
+        Rectangle rectangle = new Rectangle(x, y, width, height);
+
+        float space = 20f;
+        helpNode = HelpUtils.buildNode(dim, rectangle, guiFont, space);
+        guiNode.attachChild(helpNode);
+    }
+
+    /**
+     * Process an action that wasn't handled by the active input mode.
+     *
+     * @param actionString textual description of the action (not null)
+     * @param ongoing true if the action is ongoing, otherwise false
+     * @param tpf time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void onAction(String actionString, boolean ongoing, float tpf) {
+        if (ongoing) {
+            switch (actionString) {
+                case "dump viewport":
+                    dumper.dump(viewPort);
+                    return;
+                case "toggle help":
+                    toggleHelp();
+                    return;
+                case "toggle pause":
+                    togglePause();
+                    return;
+                case "toggle skeleton":
+                    toggleSkeleton();
+                    return;
+            }
+        }
+
+        super.onAction(actionString, ongoing, tpf);
+    }
+
+    /**
+     * Callback invoked once per frame.
+     *
+     * @param tpf the time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+        updateStatusLine();
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add a horizontal square to the scene.
+     */
+    private void addFloor() {
+        float x1 = -2f;
+        float x2 = 2f;
+        float y1 = -2f;
+        float y2 = 2f;
+        float zNorm = -1f;
+        Mesh squareMesh = new RectangleMesh(x1, x2, y1, y2, zNorm);
+        Geometry floor = new Geometry("floor", squareMesh);
+        floor.rotate(FastMath.HALF_PI, 0f, 0f);
+
+        ColorRGBA floorColor = new ColorRGBA(0.2f, 0.4f, 0.1f, 1f);
+        Material floorMaterial
+                = MyAsset.createShadedMaterial(assetManager, floorColor);
+        floorMaterial.setName("floorMaterial");
+        floor.setMaterial(floorMaterial);
+        rootNode.attachChild(floor);
+    }
+
+    /**
+     * Add lighting and shadows to the scene.
+     */
+    private void addLighting() {
+        ColorRGBA ambientColor = new ColorRGBA(0.2f, 0.2f, 0.2f, 1f);
+        AmbientLight ambient = new AmbientLight(ambientColor);
+        rootNode.addLight(ambient);
+        ambient.setName("ambient");
+
+        Vector3f direction = new Vector3f(1f, -2f, -2f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(direction);
+        rootNode.addLight(sun);
+        sun.setName("sun");
+
+        DirectionalLightShadowRenderer dlsr
+                = new DirectionalLightShadowRenderer(assetManager, 4_096, 3);
+        dlsr.setLight(sun);
+        dlsr.setShadowIntensity(0.5f);
+        viewPort.addProcessor(dlsr);
+    }
+
+    /**
+     * Configure the camera during startup.
+     */
+    private void configureCamera() {
+        float near = 0.002f;
+        float far = 20f;
+        MyCamera.setNearFar(cam, near, far);
+
+        flyCam.setDragToRotate(true);
+        flyCam.setMoveSpeed(3f);
+
+        cam.setName("cam");
+        cam.setLocation(new Vector3f(-1.7f, 1.9f, 1.2f));
+        cam.setRotation(new Quaternion(0.1044f, 0.86585f, -0.20378f, 0.44483f));
+
+        CameraOrbitAppState orbitState
+                = new CameraOrbitAppState(cam, "orbitLeft", "orbitRight");
+        stateManager.attach(orbitState);
+    }
+
+    /**
+     * Configure the PhysicsDumper during startup.
+     */
+    private void configureDumper() {
+        dumper.setDumpTransform(true);
+    }
+
+    /**
+     * Toggle visibility of the helpNode.
+     */
+    private void toggleHelp() {
+        if (helpNode.getCullHint() == Spatial.CullHint.Always) {
+            helpNode.setCullHint(Spatial.CullHint.Never);
+        } else {
+            helpNode.setCullHint(Spatial.CullHint.Always);
+        }
+    }
+
+    /**
+     * Toggle the animation: paused/running.
+     */
+    private void togglePause() {
+        float newSpeed = (speed > 1e-12f) ? 1e-12f : 1f;
+        setSpeed(newSpeed);
+    }
+
+    /**
+     * Toggle the SkeletonVisualizer on/off.
+     */
+    private void toggleSkeleton() {
+        boolean enabled = sv.isEnabled();
+        sv.setEnabled(!enabled);
+    }
+
+    /**
+     * Update the status line in the GUI.
+     */
+    private void updateStatusLine() {
+        boolean isPaused = (speed <= 1e-12f);
+        String message = isPaused ? "  PAUSED" : "";
+        statusLine.setText(message);
+    }
+}
