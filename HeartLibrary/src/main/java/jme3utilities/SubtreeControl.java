714e0c888e455d66c018569dd2684f0583ccdc21
Stephen Gold <sgold@sonic.net>
2021-11-23 14:50:34 -0800

    SubtreeControl: add a TODO comment

--- HeartLibrary/src/main/java/jme3utilities/SubtreeControl.java
+++ HeartLibrary/src/main/java/jme3utilities/SubtreeControl.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2014-2020, Stephen Gold
+ Copyright (c) 2014-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -67,7 +67,7 @@ abstract public class SubtreeControl extends SimpleControl {
     /**
      * Instantiate a disabled Control.
      */
-    public SubtreeControl() {
+    public SubtreeControl() { // TODO protect this constructor
         super.setEnabled(false);
     }
     // *************************************************************************
