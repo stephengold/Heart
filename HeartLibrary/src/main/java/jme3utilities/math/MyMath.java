e0683068621cc730873108f23818bcaec9e79dc1
Stephen Gold <sgold@sonic.net>
2023-08-07 20:32:24 -0700

    MyMath:  javadoc correction

--- HeartLibrary/src/main/java/jme3utilities/math/MyMath.java
+++ HeartLibrary/src/main/java/jme3utilities/math/MyMath.java
@@ -699,7 +699,7 @@ public class MyMath { // TODO finalize the class
      *
      * @param iValues the input values
      * @return the most positive value
-     * @see Collections#max()
+     * @see java.util.Collections#max(java.util.Collection)
      * @see java.lang.Math#max(int, int)
      */
     public static int maxInt(int... iValues) {
